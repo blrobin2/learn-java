16a08d314a8c3b991b67bc5a5fcf98f852a41647
Bennie <brobinson88@gmail.com>
2018-07-26 08:38:16 -0400

    Search tree traversal and add

--- AbstractionChallenge/src/com/blrobin2/Main.java
+++ AbstractionChallenge/src/com/blrobin2/Main.java
@@ -2,33 +2,35 @@ package com.blrobin2;
 
 public class Main {
     public static void main(String[] args) {
-        LinkedList list = new LinkedList(null);
-        list.traverse(list.getRoot());
+        SearchTree tree = new SearchTree(null);
+        tree.traverse(tree.getRoot());
         String stringData = "5 7 3 9 8 2 1 0 4 6";
         for (String s : stringData.split(" ")) {
-            list.addItem(new Node(s));
+            tree.addItem(new Node(s));
         }
 
-        list.traverse(list.getRoot());
-        list.removeItem(new Node("3"));
-        list.traverse(list.getRoot());
+        tree.traverse(tree.getRoot());
 
-        list.removeItem(new Node("5"));
-        list.traverse(list.getRoot());
-
-        list.removeItem(new Node("0"));
-        list.removeItem(new Node("4"));
-        list.removeItem(new Node("2"));
-        list.traverse(list.getRoot());
-
-        list.removeItem(new Node("9"));
-        list.traverse(list.getRoot());
-        list.removeItem(new Node("8"));
-        list.traverse(list.getRoot());
-        list.removeItem(new Node("6"));
-        list.removeItem(list.getRoot());
-        list.traverse(list.getRoot());
-        list.removeItem(list.getRoot());
-        list.traverse(list.getRoot());
+//        tree.traverse(tree.getRoot());
+//        tree.removeItem(new Node("3"));
+//        tree.traverse(tree.getRoot());
+//
+//        tree.removeItem(new Node("5"));
+//        tree.traverse(tree.getRoot());
+//
+//        tree.removeItem(new Node("0"));
+//        tree.removeItem(new Node("4"));
+//        tree.removeItem(new Node("2"));
+//        tree.traverse(tree.getRoot());
+//
+//        tree.removeItem(new Node("9"));
+//        tree.traverse(tree.getRoot());
+//        tree.removeItem(new Node("8"));
+//        tree.traverse(tree.getRoot());
+//        tree.removeItem(new Node("6"));
+//        tree.removeItem(tree.getRoot());
+//        tree.traverse(tree.getRoot());
+//        tree.removeItem(tree.getRoot());
+//        tree.traverse(tree.getRoot());
     }
 }
