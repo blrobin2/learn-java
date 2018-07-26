fab6a63a91c96c3a6eda8e9d77dc6dcabd266ca9
Bennie <brobinson88@gmail.com>
2018-07-26 14:05:31 -0400

    Generify Teams and introduce Comparable interface

--- Generics/src/com/blrobin2/Main.java
+++ Generics/src/com/blrobin2/Main.java
@@ -1,19 +1,58 @@
 package com.blrobin2;
 
 import java.util.ArrayList;
+import java.util.Collections;
 
 public class Main {
 
     public static void main(String[] args) {
-        Player joe = new FootballPlayer("Joe");
-        Player pat = new BaseballPlayer("Pat");
-        Player beckham = new SoccerPlayer("Beckham");
+        FootballPlayer joe = new FootballPlayer("Joe");
+        BaseballPlayer pat = new BaseballPlayer("Pat");
+        SoccerPlayer beckham = new SoccerPlayer("Beckham");
 
-        Team adelaideCrows = new Team("Adelaide Crows");
+        Team<FootballPlayer> adelaideCrows = new Team<>("Adelaide Crows");
         adelaideCrows.addPlayer(joe);
-        adelaideCrows.addPlayer(pat);
-        adelaideCrows.addPlayer(beckham);
+        //adelaideCrows.addPlayer(pat);
+        //adelaideCrows.addPlayer(beckham);
+
+        Team<BaseballPlayer> baseballTeam = new Team<>("Chicago Cubs");
+        baseballTeam.addPlayer(pat);
 
         System.out.println(adelaideCrows.numPlayers());
+
+        // By restricting T to Player, this won't work
+        //Team<String> brokenTeam = new Team<>("this won't work");
+        //brokenTeam.addPlayer("no one");
+
+        Team<SoccerPlayer> soccerTeam = new Team<>("soccer");
+        soccerTeam.addPlayer(beckham);
+
+        Team<FootballPlayer> melbourne = new Team<>("Melbourne");
+        FootballPlayer gordon = new FootballPlayer("Gordon");
+        melbourne.addPlayer(gordon);
+
+        Team<FootballPlayer> hawthorne = new Team<>("Hawthorne");
+        Team<FootballPlayer> fremantele = new Team<>("Fremantele");
+
+        hawthorne.matchResult(fremantele, 1, 0);
+        hawthorne.matchResult(adelaideCrows, 3, 8);
+
+        adelaideCrows.matchResult(fremantele, 1, 0);
+        //adelaideCrows.matchResult(baseballTeam, 1, 1);
+
+        System.out.println("Rankings:");
+        System.out.println(adelaideCrows.getName() + ": " + adelaideCrows.ranking());
+        System.out.println(melbourne.getName() + ": " + melbourne.ranking());
+        System.out.println(fremantele.getName() + ": " + fremantele.ranking());
+        System.out.println(hawthorne.getName() + ": " + hawthorne.ranking());
+
+        System.out.println(adelaideCrows.compareTo(melbourne));
+        System.out.println(adelaideCrows.compareTo(hawthorne));
+
+        System.out.println(hawthorne.compareTo(adelaideCrows));
+        System.out.println(melbourne.compareTo(fremantele));
+
+        //ArrayList<Team> teams;
+        //Collections.sort(teams);
     }
 }
--- Generics/src/com/blrobin2/Team.java
+++ Generics/src/com/blrobin2/Team.java
@@ -2,16 +2,16 @@ package com.blrobin2;
 
 import java.util.ArrayList;
 
-public class Team {
+public class Team<T extends Player> implements Comparable<Team<T>> {
     private String name;
     int played = 0;
     int won = 0;
     int lost = 0;
     int tied = 0;
 
-    private ArrayList<Player> members = new ArrayList<>();
+    private ArrayList<T> members = new ArrayList<>();
 
-    public Team(String name) {
+    Team(String name) {
         this.name = name;
     }
 
@@ -19,13 +19,13 @@ public class Team {
         return name;
     }
 
-    public boolean addPlayer(Player player) {
+    boolean addPlayer(T player) {
         if (members.contains(player)) {
             System.out.println(player.getName() + " is already on this team");
             return false;
         }
         members.add(player);
-        System.out.println(player.getName() + " picked for team  " + this.name);
+        System.out.println(player.getName() + " picked for team " + this.name);
         return true;
     }
 
@@ -33,21 +33,31 @@ public class Team {
         return this.members.size();
     }
 
-    void matchResult(Team opponent, int ourScore, int theirScore) {
+    void matchResult(Team<T> opponent, int ourScore, int theirScore) {
+        String message;
         if (ourScore > theirScore) {
             won++;
+            message = " beat ";
         } else if (ourScore == theirScore) {
             tied++;
+            message = " drew with ";
         } else {
             lost++;
+            message = " lost to ";
         }
         played++;
         if (opponent != null) {
+            System.out.println(this.getName() + message + opponent.getName());
             opponent.matchResult(null, theirScore, ourScore);
         }
     }
 
     int ranking() {
         return (won * 2) + tied;
+    }
+
+    @Override
+    public int compareTo(Team<T> team) {
+        return Integer.compare(team.ranking(), ranking());
     }
 }
