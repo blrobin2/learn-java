2be0a274edc70e6c1ae89e887917bb4484888844
Bennie <brobinson88@gmail.com>
2018-07-30 08:51:13 -0400

    Packages challenge; video solution

--- Numeric/src/com/blrobin2/numeric/Series.java
+++ Numeric/src/com/blrobin2/numeric/Series.java
@@ -1,20 +1,32 @@
 package com.blrobin2.numeric;
 
 public class Series {
-    public static int nSum(int n) {
+    public static long nSum(int n) {
         if (n <= 0) return 0;
-        return n + nSum(n - 1);
+        return (n * (n + 1)) / 2;
     }
 
-    public static int factorial(int n) {
-        if (n <= 0) return 0;
-        if (n == 1) return 1;
-        return n * factorial(n - 1);
+    public static long factorial(int n) {
+        if (n <= 0) return 1;
+        long fact = 1;
+        for (int i = 1; i <= n; i++) {
+            fact *= i;
+        }
+        return fact;
     }
 
-    public static int fibonacci(int n) {
+    public static long fibonacci(int n) {
         if (n == 0) return 0;
         if (n == 1) return 1;
-        return fibonacci(n - 1) + fibonacci(n - 2);
+        long nMinus1 = 1;
+        long nMinus2 = 0;
+        long fib = 0;
+        for (int i = 1; i < n; i++) {
+            fib = (nMinus2 + nMinus1);
+            nMinus2 = nMinus1;
+            nMinus1 = fib;
+        }
+
+        return fib;
     }
 }
